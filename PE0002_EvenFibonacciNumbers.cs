55328be79a39dcc8f5114662c77052958774cc4f
KarmBinning <44921669+KarmBinning@users.noreply.github.com>
2023-01-15 05:09:31 -0500

    Initial commit

---
+++ PE0002_EvenFibonacciNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class PE0002_EvenFibonacciNumbers
+{
+    public static void Run()
+    {
+        Console.WriteLine("Testing... PE0002_EvenFibonacciNumbers\n");
+
+
+    }
+}
